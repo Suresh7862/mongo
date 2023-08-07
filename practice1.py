abac3d7c8d6a04c3f95d555837ee60d7aaf37eb5
Suresh7862 <128031600+Suresh7862@users.noreply.github.com>
2023-08-07 18:32:19 +0530

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,3 +1,5 @@
+python -m pip install pymongo
+
 import streamlit
 import pymongo
 conn_obj=mongoclient('mongodb+srv://ssureshmhslearnpy:9ijop4Kt5jtDmy0f@cluster0.chzeolz.mongodb.net/?retryWrites=true&w=majority')
