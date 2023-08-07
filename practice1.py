21c25929da50477c75353ccf3a28ed06a4f362fd
Suresh7862 <128031600+Suresh7862@users.noreply.github.com>
2023-08-07 18:36:04 +0530

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,4 +1,4 @@
 import streamlit
 import pymongo
-conn_obj=mongoclient('mongodb+srv://ssureshmhslearnpy:9ijop4Kt5jtDmy0f@cluster0.chzeolz.mongodb.net/?retryWrites=true&w=majority')
+conn_obj=mongoclient("localhost",27017)
 print(type(conn_obj))
