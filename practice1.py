7ce4361e3c66cf64083cb07807994068c601fb29
Suresh7862 <128031600+Suresh7862@users.noreply.github.com>
2023-08-10 19:39:09 +0530

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,5 +1,6 @@
 import streamlit
-import pymongo
-conn_obj=pymongo.mongoclient(**streamlit.secrets["mongo"])
+#import pymongo
+#conn_obj=pymongo.mongoclient(**streamlit.secrets["mongo"])
+conn_obj=mongoclient(**streamlit.secrets["mongo"])
 db_obj=conn_obj['Demo']
 collection_obj=db_obj['Employee']
