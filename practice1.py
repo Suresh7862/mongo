7510627cd8f0351e059480fa1ac536ea3a21a31e
Suresh7862 <128031600+Suresh7862@users.noreply.github.com>
2023-08-10 20:25:37 +0530

    Update practice1.py

--- practice1.py
+++ practice1.py
@@ -1,6 +1,4 @@
-pip install pymongo[srv]
-'''
+#pip install pymongo[srv]
 import streamlit
 import pymongo
 conn_obj=pymongo.Mongoclient("mongodb+srv://streamlit.secrets.db_username:streamlit.secrets.db_pswd@streamlit.secrets.cluster_name.chzeolz.mongodb.net/?retryWrites=true&w=majority")
-'''
